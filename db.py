4403220cd2dd4c018d0d26f8dc405fea441f47ea
manzanodiego <manzanodie12@gmail.com>
2025-11-24 03:24:00 -0600

    Url

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ import psycopg2.extras
 from flask import g
 
 
-DATABASE_URL = os.environ.get('postgres://root:857oq5STWMhCKnISy2mTZ1mJnjMfnwXe@localhost:5432/servicio_y6u5')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 
 def get_db():
